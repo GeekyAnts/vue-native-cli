0b88a98516c3d7e6213bc7c2523912018bdb9bbc
NeerajSingh <neeraj_singh47@outlook.com>
2019-04-30 14:34:05 +0530

    Update RN version

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ const constantObject = {
   regExpForValidRnDirectory: /^[$A-Z_][0-9A-Z_$]*$/i,
   crnaPackageName: "expo",
   rnPackageName: "react-native",
-  stableRNVersion: "0.58.1",
+  stableRNVersion: "0.59",
   appJsonPath: "app.json",
   vueNativePackages: {
     vueNativeCore: "vue-native-core",
